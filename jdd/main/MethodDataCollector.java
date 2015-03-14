cdf354855c16806a0ceed5cf6b7407c981c42a23
Pushkar N Kulkarni <pushkar.rvce@gmail.com>
2015-03-15 00:55:05 +0530

    removing unsigned

--- jdd/main/MethodDataCollector.java
+++ jdd/main/MethodDataCollector.java
@@ -9,7 +9,7 @@ public interface MethodDataCollector
     void setMaxStack ( int maxStack );
     void setArgumentsAndTempCount ( int arguments, int temp );
     void setClassName ( String className );
-    void setModifiers ( unsigned int modifiers ); 
+    void setModifiers ( int modifiers ); 
     void addACaughtExceptionType ( int start, int end, int handler, String exceptionName );
     void addAThrownException ( String exceptionName );
     void addAJVMInstruction ( int byteCode, int bcType, int bcLength, int pc , Object[] operands );
